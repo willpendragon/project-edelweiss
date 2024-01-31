144c2c5ddaa03860dcfcc6dc745c2a668c97f791
moonchld <degregori.guglielmo@gmail.com>
2024-01-31 17:29:37 +0100

    AOE Spells Logic Development
    Created AOE Spells Enter/Exit Conditions for the Player.
    Bug Fixed: Unable to use Single Target Attacks.

--- Assets/Code/Scripts/TileController.cs
+++ Assets/Code/Scripts/TileController.cs
@@ -271,7 +271,7 @@ public class TileController : MonoBehaviour, IPointerClickHandler
         else if (currentSingleTileStatus == SingleTileStatus.attackSelectionModeWaitingForConfirmation)
         {
             //Play VFX Feedback on Enemy Target
-            OnTileConfirmedAOESpellMode();
+            OnTileConfirmedAttackMode();
             OnUpdateEnemyTargetUnitProfile(detectedUnit);
         }
         else if (currentSingleTileStatus == SingleTileStatus.aoeAttackSelectionModeActive)
@@ -291,6 +291,9 @@ public class TileController : MonoBehaviour, IPointerClickHandler
             {
                 tile.GetComponentInChildren<MeshRenderer>().material.color = Color.black;
             }
+            epicenterTargetTileController.gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.red;
+            int epicenterTargetLayer = LayerMask.NameToLayer("Epicenter Target Layer");
+            epicenterTargetTileController.gameObject.layer = epicenterTargetLayer;
             Debug.Log("Selected AOE Spell Area");
             //}
         }
@@ -304,7 +307,16 @@ public class TileController : MonoBehaviour, IPointerClickHandler
     {
         //This is the logic that happens when the Player clicks the Right Mouse Button and is usually employed to deselect Units, whether Units or Enemies.
         Debug.Log("Pressed Right Click on Tile");
-        if (currentSingleTileStatus == SingleTileStatus.selectedPlayerUnitOccupiedTile)
+        if (detectedUnit == null)
+        {
+            Debug.Log("No Unit Found");
+            if (currentSingleTileStatus == SingleTileStatus.aoeAttackSelectionModeWaitingForConfirmation)
+            {
+                DeselectAOESpellRange();
+                Debug.Log("No Unit Found. Deselecting AOE Spell range");
+            }
+        }
+        else if (currentSingleTileStatus == SingleTileStatus.selectedPlayerUnitOccupiedTile)
         //What happens if the Player clicks with the Right button when the Tile is marked as occupied by a SELECTED (Active) Player Unit
         {
             if (detectedUnit.tag == "ActivePlayerUnit" && detectedUnit.GetComponent<UnitSelectionController>().currentUnitSelectionStatus != UnitSelectionController.UnitSelectionStatus.unitAttacking)
@@ -374,12 +386,44 @@ public class TileController : MonoBehaviour, IPointerClickHandler
                 GridManager.Instance.currentPlayerUnit.GetComponent<UnitSelectionController>().currentUnitSelectionStatus = UnitSelectionController.UnitSelectionStatus.unitSelected;
                 Destroy(GameObject.FindGameObjectWithTag("TargetedEnemyUnitProfile"));
             }
+            else if (currentSingleTileStatus == SingleTileStatus.aoeAttackSelectionModeWaitingForConfirmation)
+            {
+                DeselectAOESpellRange();
+                Debug.Log("Deselecting AOE Spell range");
+            }
             else
             {
-                Destroy(GameObject.FindGameObjectWithTag("EnemyUnitProfile"));
-                //OnDeselectedTileWithUnit();
-                Debug.Log("Found Enemy Unit. Resetting Enemy character profile panel");
+                if (currentSingleTileStatus != SingleTileStatus.aoeAttackSelectionModeWaitingForConfirmation)
+                {
+                    Destroy(GameObject.FindGameObjectWithTag("EnemyUnitProfile"));
+                    //OnDeselectedTileWithUnit();
+                    Debug.Log("Found Enemy Unit. Resetting Enemy character profile panel");
+                }
             }
+        }
+        else if (detectedUnit.tag == "Player")
+        {
+            if (currentSingleTileStatus == SingleTileStatus.aoeAttackSelectionModeWaitingForConfirmation)
+            {
+                DeselectAOESpellRange();
+                Debug.Log("Found Player on AOE range. Deselecting AOE Spell range");
+            }
+        }
+        else if (detectedUnit.tag == "ActivePlayerUnit")
+        {
+            if (currentSingleTileStatus == SingleTileStatus.aoeAttackSelectionModeWaitingForConfirmation)
+            {
+                DeselectAOESpellRange();
+                Debug.Log("Found Active Player Unit on AOE range. Deselecting AOE Spell range");
+            }
+        }
+    }
+    public void DeselectAOESpellRange()
+    {
+        foreach (var tile in GridManager.Instance.gridTileControllers)
+        {
+            tile.currentSingleTileStatus = SingleTileStatus.characterSelectionModeActive;
+            tile.GetComponentInChildren<MeshRenderer>().material.color = Color.yellow;
         }
     }
     public void SwitchTileToSelectionMode()
@@ -388,151 +432,3 @@ public class TileController : MonoBehaviour, IPointerClickHandler
         Debug.Log("Switching tiles to selection Mode");
     }
 }
-
-/*
-     //The Player left clicks on a Tile while the Tile is undergoing MOVE SELECTION Mode, calls the method to move to that Tile.
-     if (Input.GetMouseButton(0) && currentSingleTileStatus == SingleTileStatus.selectionModeActive)
-     {
-            OnTileClicked?.Invoke(tileXCoordinate, tileYCoordinate);
-     }
-     //The Player left clicks on a Tile while Character Selection Mode is Active
-     if (Input.GetMouseButton(0) && currentSingleTileStatus == SingleTileStatus.characterSelectionModeActive)
-     {
-         //Tile should have a Detected Unit
-         if (detectedUnit != null)
-         {
-             //If the Detected Unit is a Player Unit
-             if (detectedUnit.gameObject.tag == "Player")
-             {
-                 //If the Grid Manager has not currently an Active Player Unit
-                 if (GridManager.Instance.currentPlayerUnit == null)
-                 {
-                     GameObject newCurrentlySelectedUnitPanel = Instantiate(currentlySelectedUnitPanel, GameObject.FindGameObjectWithTag("BattleInterfaceCanvas").transform);
- newCurrentlySelectedUnitPanel.tag = "ActiveCharacterUnitProfile";
-                     newCurrentlySelectedUnitPanel.GetComponent<HorizontalLayoutGroup>().childAlignment = TextAnchor.LowerLeft;
-                     Debug.Log("Clicked on a Tile with Player Unit on it");
-                     //Unit becomes the Active Player Unit in the GridManager
-                     GridManager.Instance.currentPlayerUnit = detectedUnit;
-                     //The Unit tag becomes ActivePlayerUnit
-                     detectedUnit.tag = "ActivePlayerUnit";
-                     //The UI shows the detected Unit details
-                     OnClickedTileWithUnit(detectedUnit);
- currentSingleTileStatus = SingleTileStatus.selectedPlayerUnitOccupiedTile;
-                     //Gameplay and Spells Buttons are generatedsi
-                     detectedUnit.GetComponent<UnitSelectionController>().GenerateGameplayButtons();
- detectedUnit.GetComponent<SpellUIController>().PopulateCharacterSpellsMenu(detectedUnit);
-}
-             }
-             else if (detectedUnit.gameObject.tag == "Enemy")
-{
- GameObject newCurrentlySelectedUnitPanel = Instantiate(currentlySelectedUnitPanel, GameObject.FindGameObjectWithTag("BattleInterfaceCanvas").transform);
- newCurrentlySelectedUnitPanel.GetComponent<HorizontalLayoutGroup>().childAlignment = TextAnchor.LowerRight;
- newCurrentlySelectedUnitPanel.tag = "EnemyUnitProfile";
- OnClickedTileWithUnit(detectedUnit);
- //Just displays the profile of the Enemy Unit sitting on the Clicked Tile
- Debug.Log("Clicked on Enemy Unit");
-}
-
-else if (detectedUnit.GetComponent<UnitSelectionController>().currentUnitSelectionStatus == UnitSelectionController.UnitSelectionStatus.unitWaiting)
-{
- Debug.Log("This Player Unit is currently in Waiting Mode");
-}
-         }
-     }
-
-     //The Player left clicks on a Tile with an Enemy on it while ATTACK SELECTION MODE (aka, Targeting during Spells) is Active
-     else if (Input.GetMouseButton(0) && currentSingleTileStatus == SingleTileStatus.attackSelectionModeActive)
-{
- //The System retrieves the coordinates from the Grid and sets the corresponding Unit as the Target
- Unit targetUnit = OnTileClickedAttackMode?.Invoke(tileXCoordinate, tileYCoordinate);
- if (targetUnit != null && targetUnit.tag == "Enemy")
- {
-     //The System retrieves the Tile Controller Instance from the Target Unit
-     TileController targetUnitTileController = GridManager.Instance.GetTileControllerInstance(tileXCoordinate, tileYCoordinate);
-     //The Tile's mode of the Target Unit and only that tile (in Single Target) becomes a Target Waiting for Confirmation
-     targetUnitTileController.currentSingleTileStatus = SingleTileStatus.attackSelectionModeWaitingForConfirmation;
-     //The Target Unit's tile turns to Red.
-     targetUnitTileController.gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.red;
-     //A Red selector icon spawns over the Target Unit's head.
-     GameObject newTargetIcon = Instantiate(targetIcon, targetUnit.transform.localPosition + (targetUnit.transform.up * 2), Quaternion.identity);
-     Debug.Log("Set Unit" + targetUnit.gameObject.name + " as current Target");
- }
-}
-//If the Target is already waiting for confirmation, I can attack that target.
-else if (Input.GetMouseButton(0) && currentSingleTileStatus == SingleTileStatus.attackSelectionModeWaitingForConfirmation)
-{
- OnTileConfirmedAttackMode();
-}
- }
- //This is the logic that happens when the Mouse is Over a Tile and is usually employed to deselect Units, whether Units or Enemies.
- public void OnMouseOver()
-{
- if (Input.GetMouseButton(1) && currentSingleTileStatus == SingleTileStatus.selectedPlayerUnitOccupiedTile)
- //What happens if the Player is standing with the Mouse and clicks with the Right button when the Tile is marked as occupied by a SELECTED (Active) Player Unit
- {
-     if (detectedUnit.tag == "ActivePlayerUnit")
-     {
-         Debug.Log("Active Player Unit Selection. Resets the Active Player Unit Selection");
-         GridManager.Instance.currentPlayerUnit = null;
-         detectedUnit.tag = "Player";
-         //Sends a message that resets the Unit Profile UI 23012024 Edited out
-         //OnDeselectedTileWithUnit();
-         Destroy(GameObject.FindGameObjectWithTag("ActiveCharacterUnitProfile"));
-         currentSingleTileStatus = SingleTileStatus.characterSelectionModeActive;
-         detectedUnit.GetComponent<UnitSelectionController>().ResetUnitSelection();
-     }
-     else if (detectedUnit.GetComponent<UnitSelectionController>().currentUnitSelectionStatus == UnitSelectionController.UnitSelectionStatus.unitWaiting)
-     {
-         OnDeselectedTileWithUnit();
-         Debug.Log("This Unit is already in Waiting Mode. Resetting Waiting Unit character profile panel");
-     }
- }
-
- else if (Input.GetMouseButton(1) && detectedUnit.tag == "Enemy")
- //What happens if the Player is standing with the Mouse and clicks with the Right Button on a Tile occupied by an Enemy 
- {
-     //During Single Target Spell Selection Mode
-     if (currentSingleTileStatus == SingleTileStatus.attackSelectionModeWaitingForConfirmation)
-     {
-         currentSingleTileStatus = SingleTileStatus.attackSelectionModeActive;
-         //The Enemy Target Icon disappears
-         GameObject[] enemyTargetIcons = GameObject.FindGameObjectsWithTag("EnemyTargetIcon");
-         foreach (GameObject enemyTargetIcon in enemyTargetIcons)
-         {
-             Destroy(enemyTargetIcon);
-         }
-     }
-     else if (currentSingleTileStatus == SingleTileStatus.attackSelectionModeActive)
-     {
-         //The Targeted Mesh changes Color to White
-         detectedUnit.GetComponent<Unit>().ownedTile.GetComponentInChildren<MeshRenderer>().material.color = Color.white;
-         //All Tiles switch back to Character Selection Mode
-         GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
-         foreach (var tile in tiles)
-         {
-             //Switches all Tiles to Character Selection Mode except the one occupied by the Active Player Unit (targeting the Enemy)
-             tile.GetComponent<TileController>().currentSingleTileStatus = SingleTileStatus.characterSelectionModeActive;
-             GridManager.Instance.currentPlayerUnit.GetComponent<Unit>().ownedTile.currentSingleTileStatus = SingleTileStatus.selectedPlayerUnitOccupiedTile;
-         }
-     }
-     else
-     {
-         Destroy(GameObject.FindGameObjectWithTag("EnemyUnitProfile"));
-         //OnDeselectedTileWithUnit();
-         Debug.Log("Found Enemy Unit. Resetting Enemy character profile panel");
-     }
- }
- else if (Input.GetMouseButton(1) && detectedUnit == null)
- {
-     Debug.Log("No Unit Found");
- }
-}
-
-
-public void SwitchTileToSelectionMode()
-{
- currentSingleTileStatus = SingleTileStatus.selectionModeActive;
- Debug.Log("Switching tiles to selection Mode");
-}
-}
-*/
