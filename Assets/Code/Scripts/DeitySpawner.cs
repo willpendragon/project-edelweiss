48a092a5b6eb81f02d6f43910f2389c7fa3ad264
moonchld <degregori.guglielmo@gmail.com>
2024-03-19 02:58:36 +0100

    Refactor DeitySpawner to use InitiateBattleWithDeity method and new Achievement Saving Behavior
    Changes:
    - Introduced InitiateBattleWithDeity(GameObject unlockedDeity) to streamline the process of spawning and initializing deity units on the battlefield. This method encapsulates the logic previously executed directly within the Start() method, improving code organization and readability.
    - Updated deity spawning logic to accept a GameObject parameter from Deity, allowing for more flexible deity initialization. This change removes the dependency on a hardcoded deity index.
    - Modified the coordinates for deity spawning to (5, 9) from (3, 7), and adjusted the deity's movement and tile occupation logic accordingly. This tweak reflects a design decision to optimize deity placement on the battlefield for enhanced gameplay experience. (These are still magic numbers to be reviewed later).

--- Assets/Code/Scripts/DeitySpawner.cs
+++ Assets/Code/Scripts/DeitySpawner.cs
@@ -11,8 +11,6 @@ public class DeitySpawner : MonoBehaviour
     [SerializeField] DeityAchievementsController deityAchievementsController;
     [SerializeField] BattleManager battleManager;
 
-    public GameObject deityBossFightSpot;
-
     public Deity currentUnboundDeity;
     // Start is called before the first frame update
     public GameObject deityHealthBar;
@@ -56,5 +54,42 @@ public class DeitySpawner : MonoBehaviour
     public void UpdateDeityHealthBar()
     {
 
+    }
+
+    public void InitiateBattleWithDeity(GameObject unlockedDeity)
+    {
+        //Unlocks Anguana as an Unbound Entity
+        //0 is a magic number, remove it later
+        Debug.Log("Unbound Anguana Unlocked");
+        unlockedDeity.GetComponent<Unit>().startingXCoordinate = 5;
+        unlockedDeity.GetComponent<Unit>().startingYCoordinate = 9;
+        GameObject unboundDeity = Instantiate(unlockedDeity);
+        Debug.Log("Moving Unbound Deity to Starting Position");
+        if (unboundDeity != null)
+        {
+            Debug.Log("Start of Summon Deity on Battlefield");
+            unboundDeity.GetComponent<Unit>().MoveUnit(5, 9);
+            //Beware, Magic Numbers
+            TileController deitySpawningTile = GridManager.Instance.GetTileControllerInstance(5, 9);
+
+            unboundDeity.GetComponent<Unit>().ownedTile = deitySpawningTile;
+
+            //Deity occupies multiple tiles
+            foreach (var deitySpawningZoneTile in GameObject.FindGameObjectWithTag("GridMovementController").GetComponent<GridMovementController>().GetMultipleTiles(deitySpawningTile))
+            {
+                deitySpawningZoneTile.currentSingleTileCondition = SingleTileCondition.occupiedByDeity;
+                deitySpawningZoneTile.GetComponentInChildren<MeshRenderer>().material.color = Color.magenta;
+                deitySpawningZoneTile.detectedUnit = unboundDeity;
+                currentUnboundDeity = unboundDeity.GetComponent<Deity>();
+                Debug.Log("Deity occupies Tile");
+            }
+
+            foreach (var enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+            {
+                Destroy(enemy);
+            }
+            unboundDeity.gameObject.tag = "Enemy";
+            CreateDeityHealthBar(unboundDeity);
+        }
     }
 }
