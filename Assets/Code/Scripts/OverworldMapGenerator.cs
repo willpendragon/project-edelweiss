ff5607834d37e3410e68225218dbb79b2f38f22c
moonchld <degregori.guglielmo@gmail.com>
2024-04-04 11:53:46 +0200

    Update OverworldMapGenerator
    - Removed zombie code from Overworld Map Generator class.

--- Assets/Code/Scripts/OverworldMapGenerator.cs
+++ Assets/Code/Scripts/OverworldMapGenerator.cs
@@ -95,26 +95,5 @@ public class OverworldMapGenerator : MonoBehaviour
                 lineRenderer.SetPosition(i, newPosition);
             }
         }
-
-        // Customize the LineRenderer, such as setting the material, if needed
-        // lineRenderer.material = ...;
     }
 }
-/*
-Vector3 initialPosition = mapNodeTransform.position; // Initial position
-Vector3 offset = new Vector3(5, 0, 0); // Example offset: adjust as needed
-int index = 0; // Keep track of the index to multiply the offset
-
-foreach (var level in levelList)
-{
-    mapNode.GetComponent<EnemySelection>().EnemyTypeIds = level.EnemyTypeIds;
-    mapNode.GetComponent<EnemySelection>().EnemyCoordinates = level.UnitCoordinates;
-    mapNode.GetComponent<EnemySelection>().levelData = level;
-
-
-    // Calculate new position with offset
-    Vector3 mapNodePosition = initialPosition + offset * index;
-    Instantiate(mapNode, mapNodePosition, Quaternion.identity);
-
-    index++; // Increment index to increase the offset for the next node
-*/
