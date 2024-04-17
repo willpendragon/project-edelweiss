ed25241edd981a301ba8156e4ac8dc67ac6cffb8
moonchld <degregori.guglielmo@gmail.com>
2024-04-17 10:56:00 +0200

    Update Level.cs
    - Removed zombie code.

--- Assets/Code/Scripts/Level.cs
+++ Assets/Code/Scripts/Level.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum EnemyType
 {
-dummy1,
-dummy2,
-dummy3
+    dummy1,
+    dummy2,
+    dummy3
 }
 
 [CreateAssetMenu(fileName = "New Level", menuName = "Level", order = 1)]
 public class Level : ScriptableObject
 {
-    //public GameObject[] enemySelection;
     public List<EnemyType> EnemyTypeIds;
     public List<Vector2> UnitCoordinates;
     public int levelNumber;
