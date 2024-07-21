ffad2748d35fe8487b72b109b45886afaf4f072e
moonchld <degregori.guglielmo@gmail.com>
2024-07-21 19:22:23 +0200

    Update AnguanHealingBehavior.cs
    - Using AnguanaHealingBehavior as her summon behaviour.
    - Adjusted in a way that triggers a protective buff on the enemy.
    - Feature still in progress, need to update feedback for all Units.

--- Assets/Code/ScriptableObjects/DeityBehaviours/AnguanaHealingBehavior.cs
+++ Assets/Code/ScriptableObjects/DeityBehaviours/AnguanaHealingBehavior.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AnguanaHealingBehavior", menuName = "DeityBehavior/AnguanaHealing")]
 public class AnguanaHealingBehavior : DeityBehavior
 {
+    float bubbleBuffShieldPointsIncreaseAmount = 9900f;
     public override void ExecuteBehavior(Deity deity)
     {
+        GameObject currentPlayerUnitGO = GridManager.Instance.currentPlayerUnit;
+
+        currentPlayerUnitGO.GetComponent<Unit>().unitShieldPoints += bubbleBuffShieldPointsIncreaseAmount;
+        currentPlayerUnitGO.GetComponentInChildren<TileShaderController>()?.AnimateFadeHeight(2.08f, 2, Color.cyan);
+
         Debug.Log("Testing Anguana Alternative Behaviour");
     }
 }
