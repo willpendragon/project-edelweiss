0a00b65c5fb6165011890ba2f7bc714a468a2529
moonchld <degregori.guglielmo@gmail.com>
2024-07-22 11:06:15 +0200

    Update UnitUIController.cs
    - Added a visual feedback triggering anytime the Player receives a buff. The class currently works with the Linked Deity Buff applied at the start of the battle (active as long summons are linked and the battle is active). Need to use the same class also for the Field Prize buffs (permanent until end of the levels).

--- Assets/Code/Scripts/UIBehaviour/UnitUIController.cs
+++ Assets/Code/Scripts/UIBehaviour/UnitUIController.cs
@@ -8,17 +8,55 @@ public class UnitUIController : MonoBehaviour
 {
     public TextMeshProUGUI unitDamageCounter;
     public Animator unitDamageCounterAnimator;
+
+    public TextMeshProUGUI unitBuffCounter;
+    public Animator unitBuffCounterAnimator;
+
+    public void OnEnable()
+    {
+        SummoningBuffController.OnAppliedDeityBuffMessage += SetBuffCounter;
+    }
+
+    public void OnDisable()
+    {
+        SummoningBuffController.OnAppliedDeityBuffMessage -= SetBuffCounter;
+    }
+
     public void SetDamageCounter(float receivedDamage)
     {
-        unitDamageCounter.text = receivedDamage.ToString();
-        unitDamageCounterAnimator.SetTrigger("activate_damage_counter");
-        unitDamageCounterAnimator.SetTrigger("reset_damage_counter");
-        StartCoroutine("ResetDamageCounter");
+        if (unitDamageCounter.text != null && unitDamageCounterAnimator != null)
+        {
+            unitDamageCounter.text = receivedDamage.ToString();
+            unitDamageCounterAnimator.SetTrigger("activate_damage_counter");
+            unitDamageCounterAnimator.SetTrigger("reset_damage_counter");
+            StartCoroutine("ResetDamageCounter");
+        }
     }
 
     IEnumerator ResetDamageCounter()
     {
-        yield return new WaitForSeconds(0.5f);
+        float damageCounterResetTime = 0.5f;
+        yield return new WaitForSeconds(damageCounterResetTime);
         unitDamageCounter.text = "";
     }
+
+
+    public void SetBuffCounter(string receivedBuff)
+    {
+        if (unitBuffCounter != null && unitBuffCounterAnimator != null)
+        {
+            unitBuffCounter.text = receivedBuff;
+            unitBuffCounterAnimator.SetTrigger("activate_buff_counter");
+            unitBuffCounterAnimator.SetTrigger("reset_buff_counter");
+            StartCoroutine("ResetBuffCounter");
+        }
+    }
+    IEnumerator ResetBuffCounter()
+    {
+        float buffCounterResetTime = 0.5f;
+        yield return new WaitForSeconds(buffCounterResetTime);
+        unitBuffCounter.text = "";
+    }
+
+
 }
