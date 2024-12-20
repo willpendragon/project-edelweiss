f8bcc2ac680702fd4923f47773c484bc442612af
moonchld <degregori.guglielmo@gmail.com>
2024-12-20 03:02:30 +0100

    Add Map Node Info System
    Now the Map Node Overlay displays correctly the icons of the enemy present in the battle and their weights.

--- Assets/MapNodeInfoPanelController.cs
+++ Assets/MapNodeInfoPanelController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,9 +8,10 @@ public class MapNodeInfoPanelController : MonoBehaviour
     [SerializeField] TextMeshProUGUI minPoolSizeText;
     [SerializeField] TextMeshProUGUI maxPoolSizeText;
     [SerializeField] GameObject mapNodeInfoPanelCanvas;
-    public GameObject[] enemyPoolPredictionsGameObject;
-
-
+    [SerializeField] GameObject[] enemyPoolPredictionsGameObject;
+    [SerializeField] Sprite enemyIcon1;
+    [SerializeField] Sprite enemyIcon2;
+    [SerializeField] Sprite enemyIcon3;
 
     private void Start()
     {
@@ -23,10 +23,12 @@ public class MapNodeInfoPanelController : MonoBehaviour
         for (int i = 0; i < enemyPoolPredictionsGameObject.Length; i++)
         {
 
-            if (i < mapNodeEnemySelection.EnemyTypeIds.Count)
+            if (i < mapNodeEnemySelection.levelData.enemyWeights.Count)
             {
                 // Assign enemy type text if within range
-                enemyPoolPredictionsGameObject[i].GetComponentInChildren<TextMeshProUGUI>().text = mapNodeEnemySelection.EnemyTypeIds[i].ToString().ToUpper();
+                enemyPoolPredictionsGameObject[i].GetComponentInChildren<TextMeshProUGUI>().text = mapNodeEnemySelection.levelData.enemyWeights[i].weight.ToString().ToUpper();
+                string enemyType = mapNodeEnemySelection.levelData.enemyWeights[i].enemyType.ToString();
+                enemyPoolPredictionsGameObject[i].GetComponent<Image>().sprite = SetEnemyIcon(enemyType);
             }
             else
             {
@@ -36,18 +38,19 @@ public class MapNodeInfoPanelController : MonoBehaviour
         }
     }
 
-    private void SetEnemyIcon(string enemyId, Image enemyIcon)
+    private Sprite SetEnemyIcon(string enemyId)
     {
         // Pass the Icon and set it
-        Image dummy1Image;
         switch (enemyId)
         {
             case "dummy1":
-                dummy1Image = enemyIcon;
-                break;
+                return enemyIcon1;
             case "dummy2":
-                break;
-
+                return enemyIcon2;
+            case "dummy3":
+                return enemyIcon3;
+            default:
+                return null;
         }
     }
 }
