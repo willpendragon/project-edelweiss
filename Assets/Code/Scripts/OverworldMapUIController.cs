cfbc29f0d6c405405aeff37ebc938d9e4fb05199
moonchld <degregori.guglielmo@gmail.com>
2025-04-28 15:11:43 +0200

    feat: Overworld Map

--- Assets/Code/Scripts/OverworldMapUIController.cs
+++ Assets/Code/Scripts/OverworldMapUIController.cs
@@ -7,10 +7,12 @@ public class OverworldMapUIController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI warFundsCounter;
     [SerializeField] TextMeshProUGUI crystalsCounter;
+    [SerializeField] GameObject locationInfoPanel;
     public GameStatsManager gameStatsManager;
     void Start()
     {
         warFundsCounter.text = gameStatsManager.warFunds.ToString();
         crystalsCounter.text = gameStatsManager.captureCrystalsCount.ToString();
+        locationInfoPanel?.SetActive(false);
     }
 }
