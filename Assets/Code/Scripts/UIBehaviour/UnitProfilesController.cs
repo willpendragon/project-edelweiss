82b97e1aced66a059336a2f3b04716edd51f67e6
moonchld <degregori.guglielmo@gmail.com>
2025-05-18 22:08:41 +0200

    feat: Create smaller Unit Profiles

--- Assets/Code/Scripts/UIBehaviour/UnitProfilesController.cs
+++ Assets/Code/Scripts/UIBehaviour/UnitProfilesController.cs
@@ -26,8 +26,9 @@ public class UnitProfilesController : MonoBehaviour
             //Spawns an information panel with Active Character Unit details on the Lower Left of the Screen
             newUnitPanel = Instantiate(Resources.Load("CurrentlySelectedUnit") as GameObject, GameObject.FindGameObjectWithTag("BattleInterfaceCanvas").transform);
             newUnitPanel.tag = "Enemy";
-            newUnitPanel.GetComponent<HorizontalLayoutGroup>().childAlignment = TextAnchor.MiddleRight;
+            newUnitPanel.GetComponent<HorizontalLayoutGroup>().childAlignment = TextAnchor.LowerCenter;
             newUnitPanel.GetComponent<HorizontalLayoutGroup>().padding.top = -376;
+            newUnitPanel.GetComponent<HorizontalLayoutGroup>().padding.left = -521;
             detectedUnit.GetComponent<Unit>().unitProfilePanel = newUnitPanel;
             newUnitPanel.GetComponent<PlayerProfileController>().ApplyProfileChanges(detectedUnit, PlayerProfileController.ProfileOwner.enemyUnit);
             newUnitPanelExists = true;
