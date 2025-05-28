562faae1d314b259647cbb3a002ce7d0cf4b5fcc
moonchld <degregori.guglielmo@gmail.com>
2025-05-29 01:27:53 +0200

    feat: Prototype Crystal Blending
    Not functional, for showcase only

--- Assets/CrystalBlendingController.cs
+++ Assets/CrystalBlendingController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,28 +9,35 @@ public class CrystalBlendingController : MonoBehaviour
     [SerializeField] GameObject crystalObject;
     [SerializeField] Slider slider;
     [SerializeField] Button[] ingredientButtons;
-    //[SerializeField] RectTransform ingredientsButtonsGrid;
     [SerializeField] Animator blendingMachine;
-    [SerializeField] Animator blendingMachineArrow;
-
-    private void Start()
-    {
-        //foreach (var button in ingredientButtons)
-        //{
-        //    Instantiate(button, ingredientsButtonsGrid);
-        //}
-    }
+    [SerializeField] TextMeshProUGUI ingredientNotification;
+    [SerializeField] string newIngredientAddedText = "New Ingredient Added";
+    [SerializeField] RectTransform blendingResultPanel;
     public void ThrowIngredient()
     {
         GameObject crystalObjectInstance = Instantiate(crystalObject);
         crystalObjectInstance.GetComponent<Animator>().SetTrigger("CrystalTossed");
+        ingredientNotification.text = newIngredientAddedText;
         slider.value += 1;
-        Destroy(crystalObjectInstance, 5);
+        float destroyDelay = 5f;
+        Destroy(crystalObjectInstance, destroyDelay);
+        StartCoroutine(ResetStartText(2f));
+    }
+    IEnumerator ResetStartText(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        ingredientNotification.text = null;
     }
 
     public void BlendIngredients()
     {
         blendingMachine.SetTrigger("CrystalBlending");
-        blendingMachineArrow.SetTrigger("CrystalBlending");
+        StartCoroutine(DisplayRecipeResultAnimation(3f));
+    }
+
+    IEnumerator DisplayRecipeResultAnimation(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        blendingResultPanel.localScale = Vector3.one;
     }
 }
