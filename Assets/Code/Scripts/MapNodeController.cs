eea7861d6614d4ea3ec12574c1cd60bc4d8fbf4d
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2025-07-18 12:00:24 +0200

    Refactor Level Type Selection

--- Assets/Code/Scripts/MapNodeController.cs
+++ Assets/Code/Scripts/MapNodeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -39,32 +40,40 @@ public class MapNodeController : MonoBehaviour, IPointerClickHandler
 
     public void HandleTileSelection()
     {
-        if (type == MapNodeType.RegularBattleNode && currentLockStatus == LockStatus.levelUnlocked)
+        if (currentLockStatus == LockStatus.levelLocked)
+            return;
+
+        switch (type)
         {
-            enemySelection.SelectMapNode();
-            GameManager.Instance.currentEnemySelectionComponent = GetComponentInParent<EnemySelection>();
-            GameManager.Instance.GetComponentInChildren<SceneLoader>().ChangeScene();
-            Debug.Log("Player chose to enter in Regular Battle");
+            case MapNodeType.RegularBattleNode:
+                HandleRegularBattle();
+                break;
+            case MapNodeType.BossBattleNode:
+                HandleBossBattle();
+                break;
         }
-        else if (type == MapNodeType.BossBattleNode && currentLockStatus == LockStatus.levelUnlocked)
+    }
+    private void HandleRegularBattle()
+    {
+        enemySelection.SelectMapNode();
+        GameManager.Instance.currentEnemySelectionComponent = GetComponentInParent<EnemySelection>();
+        GameManager.Instance.GetComponentInChildren<SceneLoader>().ChangeScene();
+        Debug.Log("Player chose to enter in Regular Battle");
+    }
+    private void HandleBossBattle()
+    {
+        GameObject[] bossBattleCurrentEnemySelection = GetComponentInParent<EnemySelection>().enemySelection;
+        List<Vector2> bossBattleEnemySelectionCoords = GetComponentInParent<EnemySelection>().EnemyCoordinates;
+
+        GameManager.Instance.currentEnemySelection = bossBattleCurrentEnemySelection;
+        GameManager.Instance.currentEnemySelectionCoords = bossBattleEnemySelectionCoords;
+
+        List<Unit> playerPartyMembersInstances = GameManager.Instance.playerPartyMembersInstances;
+        for (int i = 0; i < playerPartyMembersInstances.Count; i++)
         {
-            Debug.Log("Player chose to enter in a Boss Battle");
-            GameObject[] bossBattleCurrentEnemySelection = GetComponentInParent<EnemySelection>().enemySelection;
-            List<Vector2> bossBattleEnemySelectionCoords = GetComponentInParent<EnemySelection>().EnemyCoordinates;
-
-            GameManager.Instance.currentEnemySelection = bossBattleCurrentEnemySelection;
-            GameManager.Instance.currentEnemySelectionCoords = bossBattleEnemySelectionCoords;
-
-            List<Unit> playerPartyMembersInstances = GameManager.Instance.playerPartyMembersInstances;
-            for (int i = 0; i < playerPartyMembersInstances.Count; i++)
-            {
-                playerPartyMembersInstances[i].startingXCoordinate = (int)playerUnitsBossBattleStartingCoords[i].x;
-                playerPartyMembersInstances[i].startingYCoordinate = (int)playerUnitsBossBattleStartingCoords[i].y;
-            }
-            Debug.Log("Load Boss Battle Sequence with handpicked Enemy Selection and new Player Party initial coordinates");
-            SceneManager.LoadScene("boss_battle_prototype");
-
-            //GameManager.Instance.GetComponentInChildren<SceneLoader>().ChangeScene();
+            playerPartyMembersInstances[i].startingXCoordinate = (int)playerUnitsBossBattleStartingCoords[i].x;
+            playerPartyMembersInstances[i].startingYCoordinate = (int)playerUnitsBossBattleStartingCoords[i].y;
         }
+        SceneManager.LoadScene("boss_battle_prototype");
     }
 }
