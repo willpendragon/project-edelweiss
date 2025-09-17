a15c37b34beadb527a62ac62fc83ffab89d56474
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2025-09-17 18:09:24 +0200

    Dialogue Notification
    A notification on the Map should show up when a new dialogue is unlocked

--- Assets/DialogueUnlockedNotifications.cs
+++ Assets/DialogueUnlockedNotifications.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,6 +7,9 @@ using UnityEngine;
 public class DialogueUnlockedNotifications : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _title;
+    [SerializeField] RectTransform _popupTransform;
+    [SerializeField] CanvasGroup _canvasGroup;
+
     private void OnEnable()
     {
         ConversationManager.OnDialogueUnlocked += ShowNotification;
@@ -17,6 +21,21 @@ public class DialogueUnlockedNotifications : MonoBehaviour
 
     private void ShowNotification(string title)
     {
+        _canvasGroup.alpha = 1;
+        _popupTransform.localScale = Vector3.zero;
+        _popupTransform.DOScale(Vector3.one, 0.3f)
+            .SetEase(Ease.OutBack);
         _title.text = title;
+        Invoke("HidePopup", 2f);
+    }
+
+    public void HidePopup()
+    {
+        _popupTransform.DOScale(Vector3.zero, 0.25f)
+            .SetEase(Ease.InBack)
+            .OnComplete(() =>
+            {
+                _canvasGroup.alpha = 0;
+            });
     }
 }
