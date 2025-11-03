758cd5502184be799a072b72b3ac204104322013
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2025-11-04 00:19:36 +0100

    Update/Add Battle Callouts
    - Added Angered Deity callout.
    - Updated Laurinus Cursed Garden (added missing tile in drawing).
    - Updated background in Player Units prefabs (set alpha to 1).

--- Assets/Code/Scripts/UI/Battle/BattleCalloutsController.cs
+++ Assets/Code/Scripts/UI/Battle/BattleCalloutsController.cs
@@ -1,34 +1,41 @@
+using DG.Tweening;
 using ProjectEdelweiss.Utils;
-using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BattleCalloutsController : MonoBehaviour
 {
+    [SerializeField] private GameObject _angeredDeityCallout;
     public void OnEnable()
     {
         AOESpellPlayerAction.OnSpellCriticalHit += ShowCriticalHitCallout;
+        AOESpellPlayerAction.OnDeityAngered += ShowDeityAngeredCallout;
         DeityKingLaurinusBehavior.OnUsedCursedGarden += ShowDeityAttackCallout;
     }
 
     public void OnDisable()
     {
         AOESpellPlayerAction.OnSpellCriticalHit -= ShowCriticalHitCallout;
+        AOESpellPlayerAction.OnDeityAngered -= ShowDeityAngeredCallout;
         DeityKingLaurinusBehavior.OnUsedCursedGarden -= ShowDeityAttackCallout;
     }
+
+    [SerializeField] Volume _globalVolume;
+    [SerializeField] Canvas _calloutCanvas;
 
     public void ShowCriticalHitCallout()
     {
         Unit activePlayerUnit = GameObject.FindGameObjectWithTag(GameTags.ActivePlayerUnit)?.GetComponent<Unit>();
         if (activePlayerUnit == null)
             return;
-
         if (activePlayerUnit.unitTemplate.unitCalloutPortrait == null)
             return;
 
-        GameObject battleCalloutInstance = Instantiate(activePlayerUnit.unitTemplate.unitCalloutPortrait, this.gameObject.transform);
+        GameObject battleCalloutInstance = Instantiate(activePlayerUnit.unitTemplate.unitCalloutPortrait, _calloutCanvas.transform);
         battleCalloutInstance.GetComponentInChildren<Animator>().SetTrigger(GameTags.SHOW_UNIT_CALLOUT);
         float battleCalloutDuration = 1f;
         Destroy(battleCalloutInstance, battleCalloutDuration);
+        DarkenScreen();
         SFXHelper(activePlayerUnit);
     }
 
@@ -38,10 +45,12 @@ public class BattleCalloutsController : MonoBehaviour
             return;
         if (deityUnit.unitTemplate.unitCalloutPortrait == null)
             return;
-        GameObject battleCalloutInstance = Instantiate(deityUnit.unitTemplate.unitCalloutPortrait, this.gameObject.transform);
+
+        GameObject battleCalloutInstance = Instantiate(deityUnit.unitTemplate.unitCalloutPortrait, _calloutCanvas.transform);
         battleCalloutInstance.GetComponentInChildren<Animator>().SetTrigger(GameTags.SHOW_UNIT_CALLOUT);
         float battleCalloutDuration = 1f;
         Destroy(battleCalloutInstance, battleCalloutDuration);
+        DarkenScreen();
         SFXHelper(deityUnit);
     }
 
@@ -50,7 +59,39 @@ public class BattleCalloutsController : MonoBehaviour
         // Add a dedicated SFX for the Deity in Inspector, currently using placeholder.
         if (unit.unitTemplate.unitCriticalHitVoice == null)
             return;
+
         GameObject criticalHitVoiceSFX = Instantiate(unit.unitTemplate.unitCriticalHitVoice, Camera.main.transform);
         Destroy(criticalHitVoiceSFX, unit.unitTemplate.unitCriticalHitVoice.GetComponent<AudioSource>().clip.length);
+    }
+
+    public void ShowDeityAngeredCallout()
+    {
+        if (_angeredDeityCallout == null)
+            return;
+
+        GameObject angeredDeityCalloutInstance = Instantiate(_angeredDeityCallout, _calloutCanvas.transform);
+        angeredDeityCalloutInstance.GetComponentInChildren<Animator>().SetTrigger(GameTags.SHOW_UNIT_CALLOUT);
+        float battleCalloutDuration = 1f;
+        Destroy(angeredDeityCalloutInstance, battleCalloutDuration);
+        DarkenScreen();
+    }
+
+    private void DarkenScreen()
+    {
+        Sequence volumeSequence = DOTween.Sequence();
+
+        volumeSequence.Append(DOTween.To(
+            () => _globalVolume.weight,
+            x => _globalVolume.weight = x,
+            0.1f,
+            0.5f
+        ).SetEase(Ease.InOutSine));
+
+        volumeSequence.Append(DOTween.To(
+            () => _globalVolume.weight,
+            x => _globalVolume.weight = x,
+            0f,
+            0.5f
+        ).SetEase(Ease.InOutSine));
     }
 }
