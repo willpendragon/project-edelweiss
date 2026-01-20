41243df3bfb4e5efd264f17ad8b89a8b0b4bc0b4
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-01-20 12:04:44 +0100

    Align Deity Obelisk to Grid
    Caveat: it works well during the fight, yet the Obelisk it is a bit buried at the start of the fight.
    Need to be changed programmatically later.

---
+++ Assets/Code/Scripts/Battle/Deity/DeityObeliskHelper.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class DeityObeliskHelper : MonoBehaviour
+{
+    // WARNING
+    // Band-aid fix: set the position on the z/y of the Obelisk at -0.5f/-0.55f to display it correctly on the Tile.
+}
