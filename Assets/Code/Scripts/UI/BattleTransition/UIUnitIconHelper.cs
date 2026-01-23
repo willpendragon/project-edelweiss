5e5a13df0d96da8f571138097e06d9fb3f3442be
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-01-23 02:55:19 +0100

    Add Display Buff in Altar Transition
    Also hide Deity Icon image when empty.

--- Assets/Code/Scripts/UI/BattleTransition/UIUnitIconHelper.cs
+++ Assets/Code/Scripts/UI/BattleTransition/UIUnitIconHelper.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
 
 public class UIUnitIconHelper : MonoBehaviour
 {
     [SerializeField] private Image _deityIcon;
     public Image UnitUIIcon;
+    [SerializeField] TextMeshProUGUI _buffTextDisplay;
 
+    public void Start()
+    {
+        _deityIcon.DOFade(0, 0.1f);
+    }
     public void ShowDeityIcon(Sprite deitySprite)
     {
         // Add DoTween effect
         _deityIcon.sprite = deitySprite;
+        _deityIcon.DOFade(1, 1f);
     }
 
     public void SetBuffDisplay(FoodBuff.FoodBuffType type, float value)
     {
+        _buffTextDisplay.text = $"{type}+ { value}";
         Debug.Log($"Displaying {type} buff with total value: {value}");
     }
 }
