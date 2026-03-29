905619bb5d8fcd303e08f92d79d5635f459fd1f6
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-03-29 18:35:49 +0200

    Remove zombie code + dynamic melee range retrieval

--- Assets/Code/Scripts/Battle/Actions/MeleePlayerAction.cs
+++ Assets/Code/Scripts/Battle/Actions/MeleePlayerAction.cs
@@ -5,14 +5,7 @@ public class MeleePlayerAction : MonoBehaviour, IPlayerAction<TileController>
 {
     public Unit currentTarget;
     public TileController savedSelectedTile;
-    //public int selectionLimiter = 1;
-    private int meleeRange = 2; // Move to attack SO
-
-    //public Vector2Int knockbackDirection;
-    //public int knockbackStrength = 2;
-
-    //public delegate void UsedMeleeAction(string notification);
-    //public static event UsedMeleeAction OnUsedMeleeAction;
+    private int meleeRange;
 
     public void Execute(TileController targetTile)
     {
@@ -26,14 +19,8 @@ public class MeleePlayerAction : MonoBehaviour, IPlayerAction<TileController>
         GameObject enemyObject = targetTile.detectedUnit;
         activePlayerUnit.unitTemplate.physicAttackBehavior.AttackSequence(targetTile.detectedUnit.GetComponent<Unit>(), targetTile, activePlayerUnit);
         ResetTileColours();
-        // Insert Interaction Logic here (for mirrors) - should have a dedicated behaviur
-
         activePlayerUnit.unitOpportunityPoints--;
         UpdateActivePlayerUnitProfile(activePlayerUnit);
-
-        //OnUsedMeleeAction($"{activePlayerUnit.unitTemplate.unitName} used Melee Attack");
-        //activePlayerUnit.GetComponent<BattleFeedbackController>().PlayMeleeAttackAnimation(activePlayerUnit, defender);
-        // After executing a Melee Attack, resets the Enemy initial tile (typically, shows the Movement Range - must take into account other cases in the future).
         targetTile.tileShaderController.SetTileGlowIntensity(1f);
     }
 
@@ -56,6 +43,9 @@ public class MeleePlayerAction : MonoBehaviour, IPlayerAction<TileController>
     {
         GridMovementController gridMovementController = GameObject.FindGameObjectWithTag("GridMovementController").GetComponent<GridMovementController>();
         int distance = gridMovementController.GetDistance(activePlayerUnit.ownedTile, targetTile);
+
+        meleeRange = activePlayerUnit.unitTemplate.physicAttackBehavior.GetAttackRange(); // Retrieve melee range from SO.
+
         if (distance > meleeRange)
         {
             return false;
