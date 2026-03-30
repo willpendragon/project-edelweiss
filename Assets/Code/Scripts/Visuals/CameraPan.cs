78cfe0db60878405b812da97416826e103630225
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-03-30 18:07:07 +0200

    Overworld Map Isometric Layout

--- Assets/Code/Scripts/Visuals/CameraPan.cs
+++ Assets/Code/Scripts/Visuals/CameraPan.cs
@@ -7,12 +7,12 @@ using UnityEngine.UI;
 
 public class CameraPan : MonoBehaviour
 {
-    public float panSpeed = 20f;
-    public Vector2 panLimitX; // Set the X-axis boundaries
-    public Vector2 panLimitY; // Set the Y-axis boundaries
-    public Vector2 panLimitZ; // Set the Z-axis boundaries
-    public float margin = 10f; // Margin in pixels from the edge of the screen
-    public Camera currentCamera;
+    //public float panSpeed = 20f;
+    //public Vector2 panLimitX; // Set the X-axis boundaries
+    //public Vector2 panLimitY; // Set the Y-axis boundaries
+    //public Vector2 panLimitZ; // Set the Z-axis boundaries
+    //public float margin = 10f; // Margin in pixels from the edge of the screen
+    //public Camera currentCamera;
 
     [SerializeField] private GameObject _enterCafePanelObject;
     [SerializeField] private GameObject _enterAltarPanelObject;
@@ -25,37 +25,37 @@ public class CameraPan : MonoBehaviour
 
     void Update()
     {
-        if (panIsActive == false)
-            return;
-        Vector3 pos = transform.position;
+        //if (panIsActive == false)
+        //    return;
+        //Vector3 pos = transform.position;
 
-        if (Input.mousePosition.x >= Screen.width - margin)
-        {
-            pos.x += panSpeed * Time.deltaTime;
-            CloseBuildingMenu();
-            SetLeftArrowTransparency(0.5f);
-            SetRightArrowTransparency(1f);
-        }
-        else if (Input.mousePosition.x <= margin)
-        {
-            pos.x -= panSpeed * Time.deltaTime;
-            CloseBuildingMenu();
-            SetLeftArrowTransparency(1f);
-            SetRightArrowTransparency(0.5f);
-        }
-        else
-        {
-            SetLeftArrowTransparency(0.2f);
-            SetRightArrowTransparency(0.2f);
-        }
+        //if (Input.mousePosition.x >= Screen.width - margin)
+        //{
+        //    pos.x += panSpeed * Time.deltaTime;
+        //    CloseBuildingMenu();
+        //    SetLeftArrowTransparency(0.5f);
+        //    SetRightArrowTransparency(1f);
+        //}
+        //else if (Input.mousePosition.x <= margin)
+        //{
+        //    pos.x -= panSpeed * Time.deltaTime;
+        //    CloseBuildingMenu();
+        //    SetLeftArrowTransparency(1f);
+        //    SetRightArrowTransparency(0.5f);
+        //}
+        //else
+        //{
+        //    SetLeftArrowTransparency(0.2f);
+        //    SetRightArrowTransparency(0.2f);
+        //}
 
-        // Clamp the camera position to the boundaries
-        pos.x = Mathf.Clamp(pos.x, panLimitX.x, panLimitX.y);
-        pos.z = Mathf.Clamp(pos.z, panLimitZ.x, panLimitZ.y);
-        // Adjust for camera's orientation if it's not aligned with the XZ plane
-        pos.y = Mathf.Clamp(pos.y, panLimitY.x, panLimitY.y);
+        //// Clamp the camera position to the boundaries
+        //pos.x = Mathf.Clamp(pos.x, panLimitX.x, panLimitX.y);
+        //pos.z = Mathf.Clamp(pos.z, panLimitZ.x, panLimitZ.y);
+        //// Adjust for camera's orientation if it's not aligned with the XZ plane
+        //pos.y = Mathf.Clamp(pos.y, panLimitY.x, panLimitY.y);
 
-        transform.position = pos;
+        //transform.position = pos;
     }
     private void SetLeftArrowTransparency(float transparency)
     {
@@ -73,11 +73,11 @@ public class CameraPan : MonoBehaviour
 
     void Start()
     {
-        float horizontalNodePosition = overworldMapGenerator.currentMapNodeTransform.position.x;
-        Vector3 camPosition = currentCamera.transform.position;
-        camPosition.x = horizontalNodePosition;
-        currentCamera.transform.position = camPosition;
-        panIsActive = true;
+        //float horizontalNodePosition = overworldMapGenerator.currentMapNodeTransform.position.x;
+        //Vector3 camPosition = currentCamera.transform.position;
+        //camPosition.x = horizontalNodePosition;
+        //currentCamera.transform.position = camPosition;
+        //panIsActive = true;
     }
 
     // Quick methods to close any buildings panel when the camera is panned - should be moved to its own class later
