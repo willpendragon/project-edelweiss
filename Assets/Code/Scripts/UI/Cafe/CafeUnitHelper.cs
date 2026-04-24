8ff1e9a783954c759f79e518c3fdf177d6c9e2cb
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-04-24 12:02:20 +0200

    Make actual party appear in Café
    - Swapped feedable characters with actual units (with unlockable characters) in the Café

--- Assets/Code/Scripts/UI/Cafe/CafeUnitHelper.cs
+++ Assets/Code/Scripts/UI/Cafe/CafeUnitHelper.cs
@@ -6,8 +6,11 @@ public class CafeUnitHelper : MonoBehaviour
 {
     public Unit unit;
     public int linkedUnitIndex;
+    [SerializeField] private Animator _animator;
     void Start()
     {
-        unit = GameManager.Instance.playerPartyMembersInstances[linkedUnitIndex];
+        unit = GameManager.Instance.playerPartyMembers[linkedUnitIndex];
+        _animator = unit.gameObject.GetComponentInChildren<Animator>();
+        GetComponent<Animator>().runtimeAnimatorController = _animator.runtimeAnimatorController;
     }
 }
