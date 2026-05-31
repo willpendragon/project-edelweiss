7b3fdb08db8432b5418ea501dcbf9839c147d4b0
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-05-31 17:52:03 +0200

    Fix knockback zoom 1st time

--- Assets/Code/Scripts/Battle/Camera/CameraController.cs
+++ Assets/Code/Scripts/Battle/Camera/CameraController.cs
@@ -26,6 +26,7 @@ public class CameraController : MonoBehaviour
         {
             ApplyMapCameraSettings(mapData.cameraPosition, mapData.cameraRotation, mapData.cameraZoom,
                 mapData.isOrthographic, mapData.orthographicSize);
+            ResetCameraPosition();
         }
         else
         {
