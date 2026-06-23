3ef4c5ba2b0bea7806831c939aa4efaff9a7007c
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-06-23 12:42:52 +0200

    Add Key Counter

--- Assets/Code/Scripts/UI/OverworldMap/OverworldUIManager.cs
+++ Assets/Code/Scripts/UI/OverworldMap/OverworldUIManager.cs
@@ -6,12 +6,25 @@ public class OverworldUIManager : MonoBehaviour
     public static OverworldUIManager Instance { get; private set; }
 
     [SerializeField] private TextMeshProUGUI _keyCounterText;
+    [SerializeField] private TextMeshProUGUI _miniBossKeyCounterText;
+    [SerializeField] private TextMeshProUGUI _bossKeyCounterText;
     [SerializeField] private EventsUIManager _eventsUIManager;
 
     public EventsUIManager EventsUIManager => _eventsUIManager;
+
     public void UpdateKeyCounterText(string message)
     {
         _keyCounterText.text = message;
+    }
+
+    public void UpdateMiniBossKeyCounterText(string message)
+    {
+        _miniBossKeyCounterText.text = message;
+    }
+
+    public void UpdateBossKeyCounterText(string message)
+    {
+        _bossKeyCounterText.text = message;
     }
 
     private void Awake()
