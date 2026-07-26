8b4b5be0b5884c6ffe13ffa15075e329b3762700
Guglielmo De Gregori <degregori.guglielmo@gmail.com>
2026-07-26 16:16:02 +0200

    Add Faith Points display in Café

--- Assets/Code/Scripts/UI/Cafe/CharacterProfileSmallController.cs
+++ Assets/Code/Scripts/UI/Cafe/CharacterProfileSmallController.cs
@@ -6,6 +6,7 @@ public class CharacterProfileSmallController : MonoBehaviour
     public Unit referenceUnit;
     [SerializeField] private TextMeshProUGUI _hpText;
     [SerializeField] private TextMeshProUGUI _manaText;
+    [SerializeField] private TextMeshProUGUI _faithPointsText;
     [SerializeField] private TextMeshProUGUI _attackPower;
     [SerializeField] private TextMeshProUGUI _defense;
 
@@ -13,6 +14,7 @@ public class CharacterProfileSmallController : MonoBehaviour
     {
         _hpText.text = $"HP {referenceUnit.unitHealthPoints.ToString()} / {referenceUnit.unitMaxHealthPoints.ToString()}";
         _manaText.text = $"MP {referenceUnit.unitManaPoints.ToString()} / {referenceUnit.unitMaxManaPoints.ToString()}";
+        _faithPointsText.text = $"FAITH {referenceUnit.unitFaithPoints.ToString()} / {referenceUnit.gameObject.GetComponent<Unit>().unitTemplate.unitFaithPoints.ToString()}";
 
         // Update upgrade stats
         _attackPower.text = $"ATK {referenceUnit.unitAttackPower}";
